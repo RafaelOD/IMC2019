d8ee1211aebb7ae3238b37335edea883afa1a44d
aluno <rafael.od.dias@gmail.com>
2019-03-08 18:44:26 -0300

    IMC (9Sem imagens)

--- app/src/main/java/com/example/aluno/aula3/MainActivity.java
+++ app/src/main/java/com/example/aluno/aula3/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.aluno.aula3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView Peso;
+    TextView Altura;
+    TextView IMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Peso = (TextView) findViewById(R.id.Peso);
+        Altura = (TextView) findViewById(R.id.Altura);
+        IMC = (TextView) findViewById(R.id.IMC);
+    }
+
+    public void CalcularIMC(View view) {
+        double altura, peso;
+
+        altura = Double.parseDouble(Peso.getText().toString());
+        peso =  Double.parseDouble(Altura.getText().toString());
+
+        IMC.setText("IMC: "+Double.toString(peso/(altura*altura)));
     }
 }
